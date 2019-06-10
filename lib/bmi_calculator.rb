c31db753b13a89ce21cd333a73044eef35a5a975
Rodrigo Reyes <rreyes@canaryhealth.com>
2019-06-10 18:19:14 -0500

    Initial commit

---
+++ lib/bmi_calculator.rb
@@ -0,0 +1,6 @@
+require "bmi_calculator/version"
+
+module BmiCalculator
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bmi_calculator/version.rb
@@ -0,0 +1,3 @@
+module BmiCalculator
+  VERSION = "0.1.0"
+end
